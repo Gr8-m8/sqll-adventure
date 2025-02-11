8ddf47ebeaa7b6a04027013d1232e7fb96b85d93
M L <martinn.lindbladd@gmail.com>
2025-02-11 11:47:31 +0100

    Revert "path, inventory, item table stash"
    This reverts commit d04ff76bc8b10f54400cf954f76eee715c5a9ec9.

--- game/database_table.py
+++ game/database_table.py
@@ -5,56 +5,52 @@ class Table:
     """Generic table object"""
     TABLE = "template"
     TABLEKEY = "template_id"
-    SCHEMA = f"{TABLEKEY} INTEGER NOT NULL PRIMARY KEY"
+    SCHEMA = "template_id INTEGER NOT NULL PRIMARY KEY"
 
     @classmethod
-    def INIT(cls: "Table", db: ManagerDB):
+    def INIT(self, db: ManagerDB):
         """SETUP DATABASE TABLE"""
-        db.create_table(cls.TABLE, cls.SCHEMA)
+        db.create_table(self.TABLE, self.SCHEMA)
 
     def __init__(self, table_id: str) -> None:
         self.table_id = table_id
 
-    @classmethod
-    def create(cls, db: ManagerDB) -> str:
+    @staticmethod
+    def create(db: ManagerDB) -> str:
         """new user table row"""
-        table_id = cls.len(db)+1
-        db.insert(cls.TABLE, [str(table_id)])
+        table_id = Table.len(db)+1
+        db.insert(Table.TABLE, [str(table_id)])
         db.save()
         return str(table_id)
 
-    @classmethod
-    def get(cls: "Table", db: ManagerDB, table_id: str) -> "Table":
+    @staticmethod
+    def get(db: ManagerDB, table_id: str) -> "Table":
         """current game user"""
-        attr = db.select(cls.TABLE, ['*'], {cls.TABLEKEY: table_id})[0]
-        return cls(attr[0])
+        attr = db.select(Table.TABLE, ['*'], {User.TABLEKEY: table_id})[0]
+        return Table(attr[0])
 
-    @classmethod
-    def list(cls: "Table",db: ManagerDB) -> list:
+    @staticmethod
+    def list(db: ManagerDB) -> list:
         """table entries"""
         ret = []
-        for table in db.select(cls.TABLE, ['*']):
-            ret.append(cls(table[0]))
+        for table in db.select(Table.TABLE, ['*']):
+            ret.append(Table(table[0]))
         return ret
 
-    @classmethod
-    def len(cls: "Table", db: ManagerDB) -> int:
+    @staticmethod
+    def len(db: ManagerDB) -> int:
         """len user table entries"""
-        return db.table_len(cls.TABLE)
+        return db.table_len(Table.TABLE)
 
     def id(self):
         """get id"""
         return self.table_id
 
-    def display(self) -> str:
-        """display str"""
-        return f"{self.table_id}"
-
 class User(Table):
     """user table object"""
     TABLE = "users"
     TABLEKEY = "user_id"
-    SCHEMA = f"{TABLEKEY} INTEGER NOT NULL PRIMARY KEY, name varchar(255) NOT NULL, mail varchar(255) NOT NULL"
+    SCHEMA = "user_id INTEGER NOT NULL PRIMARY KEY, name varchar(255) NOT NULL, mail varchar(255) NOT NULL"
 
     def __init__(self, user_id: str, name: str, mail: str) -> None:
         self.user_id: str = user_id
@@ -64,34 +60,34 @@ class User(Table):
     def __str__(self):
         return f"({self.user_id}, {self.name}, {self.mail})"
 
-    @classmethod
-    def create(cls: "User", db: ManagerDB, name: str, mail: str) -> str:
+    @staticmethod
+    def create(db: ManagerDB, name: str, mail: str) -> str:
         """new user table row"""
-        user_id = cls.len(db)+1
-        db.insert(cls.TABLE, [str(user_id), '"'+name+'"', '"'+mail+'"'])
+        user_id = User.len(db)+1
+        db.insert(User.TABLE, [str(user_id), '"'+name+'"', '"'+mail+'"'])
         db.save()
         return str(user_id)
 
-    @classmethod
-    def get(cls: "User", db: ManagerDB, user_id: str) -> "User":
+    @staticmethod
+    def get(db: ManagerDB, user_id: str) -> "User":
         """current game user"""
-        attr = db.select(cls.TABLE, ['*'], {cls.TABLEKEY: user_id})[0]
-        return cls(attr[0], attr[1], attr[2])
+        attr = db.select(User.TABLE, ['*'], {User.TABLEKEY: user_id})[0]
+        return User(attr[0], attr[1], attr[2])
 
-    @classmethod
-    def list(cls: "User", db: ManagerDB) -> list:
+    @staticmethod
+    def list(db: ManagerDB) -> list:
         """user table entries"""
         ret = []
-        users = db.select(cls.TABLE, ['*'])
+        users = db.select(User.TABLE, ['*'])
         if users:
             for user in users:
-                ret.append(cls(user[0], user[1], user[2]))
+                ret.append(User(user[0], user[1], user[2]))
         return ret
 
-    @classmethod
-    def len(cls: "User", db: ManagerDB) -> int:
+    @staticmethod
+    def len(db: ManagerDB) -> int:
         """len user table entries"""
-        return db.table_len(cls.TABLE)
+        return db.table_len(User.TABLE)
 
     def id(self) -> str:
         """get id"""
@@ -105,8 +101,8 @@ class Character(Table):
     """character table object"""
     TABLE = "characters"
     TABLEKEY = "character_id"
-    sqlite_SCHEMA = f"{TABLEKEY} INTEGER NOT NULL PRIMARY KEY, name varchar(255) NOT NULL, description varchar(255), location_id REFERENCES location(id), user_id REFERENCES users(id) NOT NULL"
-    SCHEMA = f"{TABLEKEY} INTEGER NOT NULL PRIMARY KEY, name varchar(255) NOT NULL, description varchar(255), location_id varchar(255), user_id varchar(255), action varchar(1023)"
+    sqlite_SCHEMA = "character_id INTEGER NOT NULL PRIMARY KEY, name varchar(255) NOT NULL, description varchar(255), location_id REFERENCES location(id), user_id REFERENCES users(id) NOT NULL"
+    SCHEMA = "character_id INTEGER NOT NULL PRIMARY KEY, name varchar(255) NOT NULL, description varchar(255), location_id varchar(255), user_id varchar(255), action varchar(1023)"
 
     def __init__(self, character_id: str, name: str, description: str, location_id: str, user_id: str, action: str) -> None:
         self.character_id = character_id
@@ -119,42 +115,42 @@ class Character(Table):
     def __str__(self):
         return f"({self.character_id}, {self.name}, {self.description}, {self.location_id}, {self.user_id}, {self.action})"
 
-    @classmethod
-    def create(cls: "Character", db: ManagerDB, name: str, description: str, user_id: str, location_id: str = 'NULL') -> str:
+    @staticmethod
+    def create(db: ManagerDB, name: str, description: str, user_id: str, location_id: str = 'NULL') -> str:
         """new character table row"""
-        character_id = cls.len(db)+1
-        db.insert(cls.TABLE, [str(character_id), '"'+name+'"', '"'+description+'"', str(location_id), str(user_id), '"New"'])
+        character_id = Character.len(db)+1
+        db.insert(Character.TABLE, [str(character_id), '"'+name+'"', '"'+description+'"', str(location_id), str(user_id), '"New"'])
         db.save()
         return str(character_id)
 
-    @classmethod
-    def get(cls: "Character", db: ManagerDB, character_id: str, user_id: str) -> "Character":
+    @staticmethod
+    def get(db: ManagerDB, character_id: str, user_id: str) -> "Character":
         """current user character"""
         attr = db.select(
-            cls.TABLE,
+            Character.TABLE,
             ['*'],
             {
-                cls.TABLEKEY: character_id,
+                Character.TABLEKEY: character_id,
                 User.TABLEKEY: user_id
             }
         )[0]
-        return cls(attr[0], attr[1], attr[2], attr[3] if attr[3] else "NULL", attr[4], attr[5])
+        return Character(attr[0], attr[1], attr[2], attr[3] if attr[3] else "NULL", attr[4], attr[5])
 
-    @classmethod
-    def list(cls: "Character", db: ManagerDB, user_id: str = None, location_id: str = None, character_id: str = None) -> list:
+    @staticmethod
+    def list(db: ManagerDB, user_id: str = None, location_id: str = None, character_id: str = None) -> list:
         """character table entries"""
         ret = []
         where = {}
         where.update({User.TABLEKEY: user_id}) if user_id else None
         where.update({Location.TABLEKEY: location_id}) if location_id else None
-        fs = f"NOT {cls.TABLEKEY}"
+        fs = f"NOT {Character.TABLEKEY}"
         where.update({fs: character_id}) if location_id else None
 
-        user_characters = db.select(cls.TABLE, ['*']) if not (user_id or location_id or character_id) else db.select(cls.TABLE, ['*'], where)
+        user_characters = db.select(Character.TABLE, ['*']) if not (user_id or location_id or character_id) else db.select(Character.TABLE, ['*'], where)
         if (user_characters):
             for character in user_characters:
                 ret.append(
-                    cls(
+                    Character(
                         character_id=character[0],
                         name=character[1],
                         description=character[2],
@@ -165,10 +161,10 @@ class Character(Table):
                 )
         return ret
 
-    @classmethod
-    def len(cls: "Character", db: ManagerDB):
+    @staticmethod
+    def len(db: ManagerDB):
         """len character table entries"""
-        return db.table_len(cls.TABLE)
+        return db.table_len(Character.TABLE)
 
     def move(self, db: ManagerDB, location_id: str) -> list:
         """change character location"""
@@ -195,7 +191,7 @@ class Location(Table):
     """location table object"""
     TABLE = "locations"
     TABLEKEY = "location_id"
-    SCHEMA = f"{TABLEKEY} INTEGER NOT NULL PRIMARY KEY, name varchar(255) NOT NULL, description varchar(255)"
+    SCHEMA = "location_id INTEGER NOT NULL PRIMARY KEY, name varchar(255) NOT NULL, description varchar(255)"
 
     def __init__(self, location_id: str, name: str, description: str) -> None:
         self.location_id = location_id
@@ -205,39 +201,39 @@ class Location(Table):
     def __str__(self):
         return f"({self.location_id}, {self.name}, {self.description})"
 
-    @classmethod
-    def create(cls: "Location", db: ManagerDB, name: str, description: str) -> str:
+    @staticmethod
+    def create(db: ManagerDB, name: str, description: str) -> str:
         """new location table row"""
-        location_id = cls.len(db)+1
-        db.insert(cls.TABLE, [str(location_id), '"'+name+'"', '"'+description+'"'])
+        location_id = Location.len(db)+1
+        db.insert(Location.TABLE, [str(location_id), '"'+name+'"', '"'+description+'"'])
         db.save()
         return str(location_id)
 
-    @classmethod
-    def list(cls: "Location", db: ManagerDB, location_id_exclude: str = None) -> list:
+    @staticmethod
+    def list(db: ManagerDB, location_id_exclude: str = None) -> list:
         """location table entries"""
         ret = []
-        fs = f"NOT {cls.TABLEKEY}"
-        locations = db.select(cls.TABLE, ['*']) if not location_id_exclude else db.select(cls.TABLE, ['*'], {fs: location_id_exclude})
+        fs = f"NOT {Location.TABLEKEY}"
+        locations = db.select(Location.TABLE, ['*']) if not location_id_exclude else db.select(Location.TABLE, ['*'], {fs: location_id_exclude})
         if locations:
             for location in locations:
-                ret.append(cls(location[0], location[1], location[2]))
+                ret.append(Location(location[0], location[1], location[2]))
         return ret
 
-    @classmethod
-    def len(cls: "Location", db: ManagerDB) -> int:
+    @staticmethod
+    def len(db: ManagerDB) -> int:
         """len location table entries"""
-        return db.table_len(cls.TABLE)
+        return db.table_len(Location.TABLE)
 
-    @classmethod
-    def get(cls: "Location", db: ManagerDB, location_id: str = None) -> "Location":
+    @staticmethod
+    def get(db: ManagerDB, location_id: str = None) -> "Location":
         """current character Location"""
         if location_id is None or location_id == "NULL":
-            return cls(-1, "None", "Nowhere")
+            return Location(-1, "None", "Nowhere")
 
-        attr = db.select(cls.TABLE, ['*'],
-                         {cls.TABLEKEY: location_id})[0]
-        return cls(attr[0], attr[1], attr[2])
+        attr = db.select(Location.TABLE, ['*'],
+                         {Location.TABLEKEY: location_id})[0]
+        return Location(attr[0], attr[1], attr[2])
 
     def id(self) -> str:
         """get id"""
